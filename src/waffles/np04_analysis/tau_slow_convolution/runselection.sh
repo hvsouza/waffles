75d4601af336f8f8800fd84162eda0e0a31baf77
hvsouza <henriquevieira.souza@gmail.com>
2024-10-16 11:43:16 +0200

    Minor updates

--- src/waffles/np04_analysis/tau_slow_convolution/runselection.sh
+++ src/waffles/np04_analysis/tau_slow_convolution/runselection.sh
@@ -7,7 +7,7 @@ if [ "$ot" != -t ]; then
     runlist=$( tail -n +2 runlists/${type}_runs.csv | cut -d , -f 1 )
 fi
 
-echo ${runlist[@]} | xargs -P${maxprocess} -n1 bash -c 'python extract_selection.py --runs "$0" -rl '${type}' -ch 11114 11116 -f'
+echo ${runlist[@]} | xargs -P${maxprocess} -n1 bash -c 'python extract_selection.py --runs "$0" -rl '${type}' -ch 11114 -f'
 
 echo ${runlist[@]} | xargs -P${maxprocess} -n1 bash -c 'python extract_selection.py --runs "$0" -rl '${type}' -ch 11225 11227 -f'
 
